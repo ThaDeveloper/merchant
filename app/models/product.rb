17d1a6f97750786fbc29ff5ac9bcb9435588814e
Justin Ndwiga <ndwigajustin@gmail.com>
2017-04-18 16:18:07 +0300

    Designing orders

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many :orders
   validates_numericality_of :price
   validates_numericality_of :stock, :only_integer => true, :greater_than_or_equal_to => 0
     #validates :stock, numericality: { only_integer: true }
